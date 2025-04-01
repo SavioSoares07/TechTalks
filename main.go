c9c1339d2e4effe22d11110e8b86faa0a5ef3654
Savio <dsaviosnogueira@gmail.com>
2025-04-01 16:14:11 -0300

    Connection created with the database

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
 	"techTalks/handlers"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
@@ -14,6 +18,25 @@ func main() {
 	// Rotas
 	http.HandleFunc("/signup", handlers.FormRegisterHandler)
 	http.HandleFunc("/signin", handlers.FormLoginHandler)
+
+
+	//Conexão com banco de dados
+	dsn := "root:savio2002@tcp(localhost:3306)/techtalks"
+	
+	db, err := sql.Open("mysql", dsn)
+	if err != nil{
+		log.Fatal("Erro ao conectar ao banco de dados", err)
+	}
+	defer db.Close()
+
+	
+	// Testar a conexão
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Erro ao conectar ao banco:", err)
+	}
+
+	fmt.Println("Conexão bem-sucedida!")
 
 
 	fmt.Println("Servidor rodando na porta 8000")
