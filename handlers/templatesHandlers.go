6b1c2c453bbffc21fcf5526083765bbb1f66be58
Savio <dsaviosnogueira@gmail.com>
2025-04-04 23:20:16 -0300

    Creted header and create other pages

--- handlers/templatesHandlers.go
+++ handlers/templatesHandlers.go
@@ -2,26 +2,39 @@ package handlers
 
 import (
 	"html/template"
+	"log"
 	"net/http"
 )
 
 //Home page
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	tmpl, _ := template.ParseFiles("templates/home.index.html")
-	tmpl.Execute(w, nil)
+	tmpl, err := template.ParseFiles("templates/home/index.html")
+	if err != nil {
+		http.Error(w, "Erro ao carregar o template", http.StatusInternalServerError)
+		log.Printf("Erro ao carregar template: %v", err)
+		return
+	}
+
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, "Erro ao executar o template", http.StatusInternalServerError)
+		log.Printf("Erro ao executar template: %v", err)
+	}
 }
 
-// Login Page
+//Login Page
 
 func FormLoginHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl, _ := template.ParseFiles("templates/signin/index.html")
 	tmpl.Execute(w, nil)
 }
 
-// Register Page
+//Register Page
 
 func FormRegisterHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl, _ := template.ParseFiles("templates/signup/index.html")
 	tmpl.Execute(w, nil)
 }
+
+//
